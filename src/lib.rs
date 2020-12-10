1e5170285988fe6c4e2af4bc7adbbccc313e28d5
Elizabeth Marston <elmarsto@microsoft.com>
2020-12-10 14:16:22 -0800

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(extends = Events)]
+    pub type App;
+    pub type Command;
+    pub type Component;
+    pub type Events;
+    pub type Modal;
+    #[wasm_bindgen(extends = Component)]
+    pub type Plugin;
+    #[wasm_bindgen(extends = Events)]
+    pub type Vault;
+    #[wasm_bindgen(extends = Component)]
+    pub type View;
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
