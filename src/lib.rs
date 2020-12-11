26e1ab05236b35bc98861f1ab27596392087821b
Elizabeth Marston <elmarsto@microsoft.com>
2020-12-10 21:27:37 -0800

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,5 @@
-use wasm_bindgen::prelude::*;
-use js_sys::{ JsString };
-use web_sys::{ HtmlElement };
-
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(extends = Events)]
-    pub type App;
-    pub type Component;
-    // TODO: declare methods from class Component in obsidian.d.ts
-    // Need load, onload,unload,onunload,add_child,remove_child,register,register_event,register_dom_event,register_scope_event,register_interval
-    pub type EventRef;
-    pub type Events;
-    #[wasm_bindgen(method)]
-    pub fn on(this: &Events, eventName: &JsString) -> EventRef;
-    #[wasm_bindgen(extends = Component)]
-    pub type Plugin;
-    #[wasm_bindgen(method, js_name = "addStatusBarItem")]
-    pub fn add_status_bar_item(this: &Plugin) -> HtmlElement;
-    #[wasm_bindgen(method, js_class = "Plugin", js_name = "addRibbonIcon")]
-    pub fn add_ribbon_icon(this: &Plugin, icon: &JsString, title: &JsString) -> web_sys::HtmlElement;
-    #[wasm_bindgen(extends = Events)]
-    pub type Vault;
-}
+mod ob_sys;
+pub use self::ob_sys::*;
 
 #[cfg(test)]
 mod tests {
